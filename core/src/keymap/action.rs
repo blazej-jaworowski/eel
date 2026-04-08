afdea08d7b8ef5ca0ca4a2f29b30e51ae760e038
Blazej Jaworowski <blazej.jaworowski@gmail.com>
2026-04-08 12:32:48 +0200

    Blanket KeyAction impl for Arc

--- core/src/keymap/action.rs
+++ core/src/keymap/action.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use crate::{Editor, Result};
 
 /// An action bound to a key sequence. Receives a reference to the editor.
@@ -12,5 +14,11 @@ where
 {
     fn call(&self, editor: &E) -> Result<()> {
         (self)(editor)
+    }
+}
+
+impl<E: Editor, A: KeyAction<E> + ?Sized> KeyAction<E> for Arc<A> {
+    fn call(&self, editor: &E) -> Result<()> {
+        (**self).call(editor)
     }
 }
